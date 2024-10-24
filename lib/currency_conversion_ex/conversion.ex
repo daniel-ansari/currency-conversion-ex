2e1c2cb7740fa7d2872432c51bec026559b30af9
Daniel Ansari <daniel.ansari.2023@gmail.com>
2024-10-24 11:22:33 +0700

    Add credo and excoveralls deps

--- lib/currency_conversion_ex/conversion.ex
+++ lib/currency_conversion_ex/conversion.ex
@@ -1,4 +1,8 @@
 defmodule CurrencyConversionEx.Conversion do
+  @moduledoc """
+  The Conversion context.
+  """
+
   @exchange_rates %{
     "USD" => %{"THB" => 32.0, "EUR" => 0.85, "GBP" => 0.76},
     "EUR" => %{"USD" => 1.18, "THB" => 37.65},
@@ -7,7 +11,7 @@ defmodule CurrencyConversionEx.Conversion do
   }
 
   def convert(amount, from_currency, to_currency) do
-  case Map.get(@exchange_rates, from_currency) do
+    case Map.get(@exchange_rates, from_currency) do
       rates ->
         case Map.get(rates, to_currency) do
           nil -> {:error, "Conversion rate not available for #{from_currency} to #{to_currency}"}
